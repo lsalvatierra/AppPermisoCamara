ba5f3b6fdccce192c97634d207a0a827c15c260c
LuisAngel <luisangel.salvat@gmail.com>
2022-05-27 17:21:31 -0500

    Initial commit

---
+++ app/src/main/java/pe/edu/idat/apppermisocamara/commom/Constantes.kt
@@ -0,0 +1,11 @@
+package pe.edu.idat.apppermisocamara.commom
+
+class Constantes {
+
+    companion object{
+        //Código de cámara request
+        const val ID_REQUEST_PERMISSION = 1991
+
+    }
+
+}
